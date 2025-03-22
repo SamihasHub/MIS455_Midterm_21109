da323c8e06e0f102302598b69d286d022661c602
Samiha <2110928@iub.edu.bd>
2025-03-22 21:24:58 +0600

    did pay bill button

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+function selfCare() {
+    var userId = prompt("Enter your UserID:");
+    var billAm = prompt("Bill Amount:");
+    var payM = prompt("Enter Payment details:");
+    
+    if (userId && billAm && payM) {
+        alert(`Your Payment is Successful!`);
+    } else {
+        alert("Invalid Input.");
+    }
+  }
